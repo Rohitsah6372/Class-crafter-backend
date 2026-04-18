becaba9f7f80a6b421e28207eb0d30be16b5906b
Rohit Sah <rohitsah402@gmail.com>
2026-04-18 15:30:49 +0530

    Added-technology-Access

--- student/urls.py
+++ student/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import academic_performance_list_create, student_list_create, parent_education_list_create, family_background_list_create, school_list_create, activity_list_create
+from .views import academic_performance_list_create, student_list_create, parent_education_list_create, family_background_list_create, school_list_create, activity_list_create, peer_group_list_create, technology_access_list_create
 
 urlpatterns = [
 
@@ -12,4 +12,6 @@ urlpatterns = [
     path('family/', family_background_list_create),
     path('schools/', school_list_create),
     path('activities/', activity_list_create),
+    path('peer-groups/', peer_group_list_create),
+    path('tech-access/', technology_access_list_create),
 ]
